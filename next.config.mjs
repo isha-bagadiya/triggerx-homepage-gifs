9a72898d0b16733e953ecad13a4d2266670e2f32
isha-bagadiya <ishabagadiya@lampros.tech>
2025-05-20 15:36:33 +0530

    changed gifs with webm format

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,3 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
@@ -12,4 +8,4 @@ const nextConfig = {
   },
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
